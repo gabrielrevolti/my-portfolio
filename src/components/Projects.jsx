3e9493ce91ebfcefdaba8623a9d64e90cfb1d85a
Gabriel Revolti <124897816+gabrielrevolti@users.noreply.github.com>
2024-09-05 17:56:19 -0300

    add motion to projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import { PROJECTS } from '../constants'
 import { MdArrowOutward } from 'react-icons/md'
+import {motion} from "framer-motion"
 
 const Projects = () => {
   return (
     <section className='pt-20 lg:mx-14' id='projects'>
-      <h2 className='mb-8 text-center text-3xl lg:text-4xl'>Projects</h2>
+      <motion.h2
+        initial={{ opacity: 0, y: -20}}
+        whileInView={{ opacity: 1, y: 0}}
+        transition={{ duration: 0.8}}
+        className='mb-8 text-center text-3xl lg:text-4xl'>
+        Projects
+      </motion.h2>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
         {PROJECTS.map((project) => (
-         <div key={project.id} className="group relative overflow-hidden rounded-3xl">
-          <img src={project.image} alt={project.name} className='h-full w-full object-cover transition-transform duration-500 group-hover:scale-110'/>
-          <div className='absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100'>
+         <motion.div
+         initial={{ opacity: 0, scale:0.9}}
+         whileInView={{opacity: 1, scale: 1}}
+         transition={{duration: 0.5}}
+         whileHover={{ scale: 1.05}}
+          key={project.id} 
+          className="group relative overflow-hidden rounded-3xl">
+          <motion.img 
+          whileHover={{scale: 1.1}}
+          src={project.image}
+          alt={project.name}
+          className='h-full w-full object-cover transition-transform duration-500 group-hover:scale-110'/>
+          <motion.div
+          initial={{opacity: 0}}
+          whileHover={{opacity: 1}}
+          transition={{duration: 0.5}}
+          className='absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100'>
             <h3 className='mb-2 text-xl'>{project.name}</h3>
             <p className='mb-12 p-4'>{project.description}</p>
             <a href={project.githubLink} target='_blank' rel='noopener noreferrer' className='rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300'>
@@ -19,8 +40,8 @@ const Projects = () => {
                 <MdArrowOutward/>
               </div>
             </a>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
         ))}
       </div>
     </section>
