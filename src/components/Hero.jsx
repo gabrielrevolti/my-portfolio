234c9b1a9a6864bc2985a3bec98305d0a2d321da
Gabriel Revolti <124897816+gabrielrevolti@users.noreply.github.com>
2024-09-05 10:56:10 -0300

    add motion to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { HERO } from '../constants'
+import {motion} from "framer-motion"
 import carlImg from "../assets/carl.jpeg"
 
 const Hero = () => {
   return (
     <section className='flex min-h-screen flex-wrap items-center  lg:mx-14 lg:mt-8'>
-      <div className='w-full md:w-1/2'>
+      <motion.div 
+      initial={{opacity: 0, x: -50}} 
+      animate={{opacity: 1, x: 0}}
+      transition={{duration: 1}}
+      className='w-full md:w-1/2'>
+      
         <h2 className='my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[7rem]'>
           {HERO.name}
         </h2>
@@ -15,12 +21,24 @@ const Hero = () => {
         <p className='mb-8 p-2 text-xl'>
           {HERO.description}
         </p>
-      </div>
-      <div className='w-full md:w-1/2 lg:p-8'>
+      </motion.div>
+      <motion.div
+      initial={{opacity: 0, scale: 0.8}}
+      animate={{opacity: 1, scale: 1}}
+      transition={{duration: 1}}
+      className='w-full md:w-1/2 lg:p-8'>
         <div className="flex justify-center">
-          <img src={carlImg} width={550} height={550} alt='Carl Frank' className='rounded-3xl' />
+          <motion.img
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{duration: 1}}
+          src={carlImg}
+          width={550}
+          height={550}
+          alt='Carl Frank'
+          className='rounded-3xl' />
         </div>
-      </div>
+      </motion.div>
     </section>
   )
 }
