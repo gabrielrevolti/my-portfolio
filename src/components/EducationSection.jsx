f05317f490818916e119cca954e53866bc6fcabc
Gabriel Revolti <124897816+gabrielrevolti@users.noreply.github.com>
2024-09-06 12:44:52 -0300

    add motion to education section

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { EDUCATION } from '../constants';
+import { motion} from "framer-motion"
 
 const EducationSection = () => {
   return (
     <section className="py-8 lg:mx-14" id='education'>
-      <h2 className="mb-4 text-center text-3xl">Education</h2>
+      <motion.h2
+        initial={{opacity: 0, y: -20 }}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 1 }}
+        className="mb-4 text-center text-3xl">Education
+      </motion.h2>
       {EDUCATION.map((edu, index) => (
-        <div key={index} className='mb-6 p-10'>
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          whileInView={{opacity: 1, x: 0}}
+          transition={{duration: 1, delay: index * 0.5}}
+          key={index} className='mb-6 p-10'>
           <h3 className="text-xl fontsemibold">{edu.degree}</h3>
           <p className="text-lg">{edu.institution}</p>
           <p className="text-sm text-stone-300">{edu.duration}</p>
           <p className="mt-2">{edu.description}</p>
-        </div>
+        </motion.div>
       ))}
     </section>
   )
