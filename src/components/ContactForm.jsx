48689d8a6294cd178ee79be0e2718a2959562dd5
Gabriel Revolti <124897816+gabrielrevolti@users.noreply.github.com>
2024-09-06 16:50:01 -0300

    add motion to contact section

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,6 +2,7 @@ import emailjs from "@emailjs/browser"
 import { useState } from "react"
 import toast, { Toaster } from "react-hot-toast"
 import { FiSend } from "react-icons/fi"
+import {motion} from "framer-motion"
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -67,20 +68,32 @@ const ContactForm = () => {
     <div className="p-4 lg:w-3/4" id="contact">
       <Toaster/>
       <h2 className="my-8 text-center text-4xl tracking-tighter">Let's Connect</h2>
-      <form onSubmit={handleSubmit}>
+      <motion.form 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 0.8, delay: 0.7}}
+        onSubmit={handleSubmit}>
         <div className="mb-4 flex space-x-4">
           <div className="lg:w-1/2">
             <input type="text" id="name" name="name" value={formData.name} placeholder="Name"
             onChange={handleChange} className="mb-8 w-full appearance-none rounded-lg border border-stone-50/30 bg-transparent px-3 py-2 text-sm focus:borderstone400 focus:outline-none"/>
             {errors.name && (
-              <p className="text-sm text-rose-800">{errors.name}</p>
+              <motion.p
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                className="text-sm text-rose-800">{errors.name}
+              </motion.p>
             )}
           </div>
           <div className="lg:w-1/2">
             <input type="email" id="email" name="email" value={formData.email} placeholder="Email"
             onChange={handleChange} className="mb-8 w-full appearance-none rounded-lg border border-stone-50/30 bg-transparent px-3 py-2 text-sm focus:borderstone400 focus:outline-none"/>
             {errors.email && (
-              <p className="text-sm text-rose-800">{errors.email}</p>
+              <motion.p
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                className="text-sm text-rose-800">{errors.email}
+              </motion.p>
             )}
           </div>
         </div>
@@ -88,7 +101,11 @@ const ContactForm = () => {
             <textarea id="message" name="message" value={formData.message} placeholder="Message"
             onChange={handleChange} className="mb-8 w-full appearance-none rounded-lg border border-stone-50/30 bg-transparent px-3 py-2 text-sm focus:borderstone400 focus:outline-none" rows="6"/>
             {errors.message && (
-              <p className="text-sm text-rose-800">{errors.message}</p>
+              <motion.p 
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                className="text-sm text-rose-800">{errors.message}
+              </motion.p>
             )}
           </div>
           <button type="submit" className={`mb-8 w-full rounded border border-stone-50/30 bg-stone-200 px-4 py-2 text-sm font-semibold text-stone-900 hover:bg-stone-300 ${isSending ? "cursor-not-allowed opacity-50" : ""}`} disabled={isSending}>
@@ -97,7 +114,7 @@ const ContactForm = () => {
               <FiSend/>
             </div>
           </button>
-      </form>
+      </motion.form>
     </div>
   )
 }
