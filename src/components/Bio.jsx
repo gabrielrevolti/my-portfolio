5396a534424c3d50f6c193064be7512d86024866
Gabriel Revolti <124897816+gabrielrevolti@users.noreply.github.com>
2024-09-06 08:46:24 -0300

    add motion to bio section

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,16 +1,26 @@
 import { BIO } from "../constants";
+import {motion} from "framer-motion"
 
 const Bio = () => {
   return (
     <section className="flex max-w-4xl flex-col gap-12 pt-20" id="bio">
-      <h2 className="text-center text-3xl lg:text-4xl">Bio</h2>
-      <div>
+      <motion.h2
+        initial={{opacity: 0, y: -20}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 1}}
+        className="text-center text-3xl lg:text-4xl">
+        Bio
+      </motion.h2>
+      <motion.div
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1}}
+      transition={{duration: 0.8, delay: 0.2}}>
         {BIO.map((bio, index) => (
           <p key={index} className="mb-4 text-lg lg:text-xl md:text-justify">
             {bio}
           </p>
         ))}
-      </div>
+      </motion.div>
     </section>
   )
 }
